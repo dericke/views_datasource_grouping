2b6cdbd262d1fafbc0e442ace77a77ce958ffc6e
Richard A. Allen <richard.alexander.allen@gmail.com>
2012-06-04 17:30:23 -0400

    Issue #1616674 by AlexanderAllen: Add "remove newline" option.

--- views/theme/views-views-json-style-simple.tpl.php
+++ views/theme/views-views-json-style-simple.tpl.php
@@ -21,6 +21,10 @@ if ($view->override_path) {
 }
 else {
   $json = _views_json_json_encode($rows, $bitmask);
+  if ($options['remove_newlines']) {
+     $json = preg_replace(array('/\\\\n/'), '', $json);
+  }
+  
   if ($jsonp_prefix) $json = "$jsonp_prefix($json)";
   if ($options['using_views_api_mode']) {
     // We're in Views API mode.
