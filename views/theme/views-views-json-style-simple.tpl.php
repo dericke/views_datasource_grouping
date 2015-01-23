751b9828190eecd00a50badee4bdfa26d61add43
Evan Derickson <derickson.e@gmail.com>
2015-01-23 00:38:32 -0800

    Allow user to set a label on a grouped field.

--- views/theme/views-views-json-style-simple.tpl.php
+++ views/theme/views-views-json-style-simple.tpl.php
@@ -11,25 +11,18 @@
  * @ingroup views_templates
  */
 
-//~ foreach ($rows["nodes"] as $k0 => $v0) {
-  //~ echo "<h1>$k0</h1>";
-  //~ foreach ($v0 as $k1 => $v1) {
-    //~ echo "<h2>$k1</h2>";
-    //~ foreach ($v1 as $k2 => $v2) {
-      //~ echo "$k2: $v2 <br />";
-    //~ }
-    //~ echo "<br />";
-  //~ }
-  //~ echo "<br />";
-//~ }
 $group = $options["grouping"][0]["field"];
+// If a label is set for the grouped field, get it and use it instead of the machine name.
+// $options uses the labeled name of the field, so we need to match that for grouping.
+$group_label = $view->query->pager->display->handler->handlers['field'][$group]->options['label'];
+if (strlen($group_label) > 0) $group = $group_label;
 $root = $options["root_object"];
 $top_child = $options["top_child_object"];
+
 if ($group) {
 $grouped = array();
   foreach ($rows[$root] as $key => $array) { // Values are numeric
     // Grab the grouping field value from inside the 3rd-level array.
-    // Creating a label for grouping field will break it for now. @TODO get the pre-labeling value of $rows
     $groupnode = $array[$top_child][$group];
     foreach ($array[$top_child] as $prop => $value) {
       if ($prop != $group) { // Ignore grouped field
@@ -40,40 +33,24 @@ $grouped = array();
 $rows = $grouped;
 }
 
-//~ echo "<hr />";
-//~ foreach ($grouped as $name => $properties) {
-  //~ echo "<h2>\$name = $name</h2>";
-  //~ foreach ($properties as $propertyk => $propertyv) {
-    //~ echo "<dt>\$propertyk = $propertyk</dt><dl><table>";
-    //~ foreach ($propertyv as $key => $value) {
-      //~ echo "<tr><td>$key</td><td>$value</td></tr>";
-    //~ }
-    //~ echo "</table></dl>";
-  //~ }
-//~ }
-
-
 // Uncomment everything below for prod
 $jsonp_prefix = $options['jsonp_prefix'];
-//~ //~
 if ($view->override_path) {
   // We're inside a live preview where the JSON is pretty-printed.
   $json = _views_json_encode_formatted($rows);
   if ($jsonp_prefix) $json = "$jsonp_prefix($json)";
   print "<code>$json</code>";
-}
-else {
+} else {
   $json = _views_json_json_encode($rows, $bitmask);
   if ($options['remove_newlines']) {
      $json = preg_replace(array('/\\\\n/'), '', $json);
   }
-//~ //~
+
   if ($jsonp_prefix) $json = "$jsonp_prefix($json)";
   if ($options['using_views_api_mode']) {
     // We're in Views API mode.
     print $json;
-  }
-  else {
+  } else {
     // We want to send the JSON as a server response so switch the content
     // type and stop further processing of the page.
     $content_type = ($options['content_type'] == 'default') ? 'application/json' : $options['content_type'];
